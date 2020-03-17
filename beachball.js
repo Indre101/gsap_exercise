441fb377d4efbe1a24c7fefae32695b8f55c340d
indre101 <zygaityte.indre@gmail.com>
2020-03-17 13:10:23 +0100

    ball moves to the top right corner

--- beachball.js
+++ beachball.js
@@ -22,11 +22,25 @@ function sequince() {
   });
 }
 
-moveAll();
 function moveAll() {
   gsap.fromTo(
     ".ball",
     { y: -400, duration: 2 },
     { y: 0, duration: 2, ease: "elastic", stagger: 0.5 }
   );
+}
+
+moveBallToCorner();
+function moveBallToCorner() {
+  tl.to(document.querySelector(".ball"), {
+    x: 400,
+    rotation: -60,
+    duration: 2
+  }).to(document.querySelector(".ball"), {
+    scale: 0.1,
+    transformOrigin: "bottom",
+    x: 1000,
+    y: -1000,
+    duration: 2
+  });
 }
