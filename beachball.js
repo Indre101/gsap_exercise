bddb30cc02d6a9efc7f948e9183a5f5f08f88dc6
indre101 <zygaityte.indre@gmail.com>
2020-03-17 10:21:39 +0100

    rotates ball

--- beachball.js
+++ beachball.js
@@ -1,1 +1,17 @@
 "use strict";
+import { gsap } from "gsap";
+
+function moveBall(params) {
+  gsap.to(document.querySelector(".ball"), {
+    transform: "translateX(400px)",
+    duration: 2
+  });
+}
+
+rotateBall();
+function rotateBall() {
+  gsap.to(document.querySelector(".ball"), {
+    transform: " rotate(-30deg) translateX(400px)",
+    duration: 2
+  });
+}
